8abc195d9b9440ba002289a850cccf062d4c860f
haln14 <haln14@fpt.com.vn>
2022-10-24 17:09:58 +0700

    Upgrade add all send data to client to handler in catch_controller

--- src/lib/catch_controller.js
+++ src/lib/catch_controller.js
@@ -1,5 +1,5 @@
 const catchController =  (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+  Promise.resolve(fn(req, res, next)).then(data => res.sendJson({data})).catch((err) => next(err));
 };
 
 export default catchController;
